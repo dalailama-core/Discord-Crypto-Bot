fc0eb9a162f0f6fe704ca694e5a72e50ef6cf7d9
Adriano Faria <jadrianofaria@gmail.com>
2021-03-27 00:09:14 +0000

    Connect coinmarketcap information with the discord bot

--- main.py
+++ main.py
@@ -1,7 +1,51 @@
 import discord
 import os
 import requests
-import myScrap
+from requests import Request, Session
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+import json
+import os
+
+ap_key=os.getenv('ap_key')
+def getCoinInfo(symbol, convertCoin,key):
+  url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
+  info=""
+  #symbol= input("symbol coin? ").upper()
+  #convertCoin=input("convert coin? ").upper()
+  parameters = {
+      'symbol':symbol,
+      'convert':convertCoin
+  }
+  headers = {
+    'Accepts': 'application/json',
+    'X-CMC_PRO_API_KEY': key
+  }
+
+  session = Session()
+  session.headers.update(headers)
+
+  try:
+    response = session.get(url, params=parameters)
+    #data = json.loads(response.text)
+    #print(data)
+    parseData = json.dumps(response.json())
+    #print(parseData)
+    ethObj = json.loads(parseData)
+    name = ethObj["data"][symbol]["name"]
+    supply = ethObj["data"][symbol]["circulating_supply"]
+    price = ethObj["data"][symbol]["quote"][convertCoin]["price"]
+    #print("Name -> ",name)
+    #print("Circulating Supply _> ",supply)
+    #print("Price -> ",price)
+    info = name+"\n"+str(supply)+"\n"+str(price)
+    print(info)
+    ethString = str(ethObj["data"][symbol]["quote"][convertCoin]["price"])
+    ethPrice = float(ethString)
+
+  except (ConnectionError, Timeout, 
+  TooManyRedirects) as e:
+    print(e)
+  return info
 
 
 client = discord.Client()
@@ -16,10 +60,11 @@ async def on_message(message):
     return
   msg = message.content
   print(message.author.name)
-  reusultado = requests.g("https://coinmarketcap.com/")
+  
   if msg.startswith('$bitcoin'):
-    #await message.channel.send("Hello!")
-    await message.author.send("ola")
+    info = getCoinInfo("BTC","EUR",ap_key)
+    await message.channel.send(info)
+   # await message.author.send("ola")
 
 client.run(os.getenv('TOKEN'))
 
